bf4f6051740b55ff5d4953a05c43fe92f62d5658
Juan Cruz Gonzalez <juancruzgonzalezsevilla@gmail.com>
2022-08-23 08:45:11 -0300

    cambios numero 6

--- src/components/navegacion/Navbar.js
+++ src/components/navegacion/Navbar.js
@@ -1,37 +1,34 @@
 import { Link } from 'react-router-dom'
-
+import './navbar.css'
 
 
 const Navbar = () => {
     return (
-    <div>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="container-fluid">
+    <>
+        <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
+            <div className='container-fluid'>
                 <Link to='/'>
                     <img src='/logo_bauzen.png' alt='imagen de bauzen' width='95'/>
                 </Link>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+                    <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
+                        <span className='navbar-toggler-icon'></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav mx-auto">
-                            <li class="nav-item">
-                                <Link class="nav-link" to='/' >inicio</Link>
+                    <div className='collapse navbar-collapse' id='navbarNav'>
+                        <ul className='navbar-nav'>
+                            <li className="nav-item">
+                                <Link className="nav-link" to='/' >inicio</Link>
                             </li>
-                            <li class="nav-item">
-                                <Link class="nav-link" to='../paginas/contacto.js'>contacto</Link>
+                            <li className='nav-item'>
+                                <Link className='nav-link' to='/'>contacto</Link>
                             </li>
-                            <li class="nav-item">
-                                <Link class="nav-link" to='../paginas/productos.js'>productos</Link>
+                            <li className='nav-item'>
+                                <Link className='nav-link' to='/'>productos</Link>
                             </li>
-                            {/* <li class="nav-item">
-                                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                            </li> */}
                         </ul>
                     </div>
             </div>
         </nav>
-    </div>
+    </>
     )
 }
 
